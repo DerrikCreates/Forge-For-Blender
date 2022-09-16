ee58ca8f38d482562316f8073aea2536c33e1e43
Derrik <heyimderrik@gmail.com>
2022-09-16 12:19:19 -0400

    very basic ui for selecting and show map data
    not currently showing map data. Look into a better way to bind buttons to paths. this feels very hacky

--- UI/Form1.Designer.cs
+++ UI/Form1.Designer.cs
@@ -30,6 +30,7 @@ partial class Form1
     private void InitializeComponent()
     {
             this.button1 = new System.Windows.Forms.Button();
+            this.FileListLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
             // 
             // button1
@@ -42,11 +43,19 @@ partial class Form1
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // FileListLayoutPanel
+            // 
+            this.FileListLayoutPanel.Location = new System.Drawing.Point(12, 41);
+            this.FileListLayoutPanel.Name = "FileListLayoutPanel";
+            this.FileListLayoutPanel.Size = new System.Drawing.Size(162, 397);
+            this.FileListLayoutPanel.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FileListLayoutPanel);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,4 +67,5 @@ partial class Form1
 
     private OpenFileDialog openFileDialog1;
     private Button button1;
+    private FlowLayoutPanel FileListLayoutPanel;
 }
--- UI/Form1.cs
+++ UI/Form1.cs
@@ -11,18 +11,41 @@ public partial class Form1 : Form
     }
 
 
+    private List<string> directoryFiles = new List<string>();
+    private Dictionary<Button, string> fileButtons = new Dictionary<Button, string>();
+
+    private void DisplayMapData(string xmlPath)
+    {
+        // ADD Displaying of map data here. mostly for testing
+        Log.Debug("diplay map data method {XMLPath}", xmlPath);
+    }
+
     private void button1_Click(object sender, EventArgs e)
     {
         FolderBrowserDialog directoryDialog = new FolderBrowserDialog();
         directoryDialog.InitialDirectory = Program.Settings.LastUsedPath;
-        
+
 
         if (directoryDialog.ShowDialog() == DialogResult.OK)
         {
             var fileInfo = new FileInfo(directoryDialog.SelectedPath);
             Program.Settings.LastUsedPath = directoryDialog.SelectedPath;
             Log.Information(directoryDialog.SelectedPath);
-            
+
+            directoryFiles = Directory.GetFiles(directoryDialog.SelectedPath, "*.xml").ToList();
+
+            foreach (var file in directoryFiles)
+            {
+                Button b = new Button();
+                b.Text = Path.GetFileName(file);
+                fileButtons.Add(b, file);
+                b.Click += new EventHandler(delegate(object? o, EventArgs args)
+                {
+                    DisplayMapData(fileButtons[(Button)o]);
+                });
+
+                FileListLayoutPanel.Controls.Add(b);
+            }
         }
     }
 }
