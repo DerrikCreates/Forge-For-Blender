1d3ced6a6d39e4801336c65e12e15870c0c89498
Derrik <heyimderrik@gmail.com>
2022-09-19 07:57:32 -0400

    basic block out support

--- Core/Blender Addon/BlenderAddon.py
+++ Core/Blender Addon/BlenderAddon.py
@@ -3,14 +3,12 @@
 
 bl_info = {
     "name": "Forge Data Exporter",
-    "author": "DerrikCreates",
+    "author": "DerrikCreates.com",
     "description": "export forge items for latter processing into a .mvar",
-    "version": (0.1),
     "blender": (3, 3, 0),
     "location": "View3D",
     "warning": "",
-    "category": "Generic",
-    "doc_url": "https://github.com/DerrikCreates/Halo-Infinite-Forge-For-Blender"
+    "category": "Generic"
 }
 
 import bpy
@@ -22,32 +20,63 @@ from bpy.types import Operator
 
 def export_item_data(context, filepath):
     print("Collecting Forge Data...")
-    test = ItemData()
-    test.rotX = 1
-
-    print(test.toJSON())
 
     depsgraph = bpy.context.evaluated_depsgraph_get()  # Get ref to the evaulated "scene"?
     objects = depsgraph.scene.objects  # get evaulated objects
 
+
+
+
+
     itemList = []
     print("------")
     for object in objects:
-        itemData = ItemData()
-        evalObject = object.evaluated_get(depsgraph)
+        if object.forge_export_toggle == True:
 
-        print("checking object " + evalObject.name)
-        print(evalObject.data.attributes)
-        if len(evalObject.data.attributes) < 2:
-            print("----" + evalObject.name + "----")
-            if len(evalObject.data.attributes) != 0:
-                for attr in evalObject.data.attributes:
-                    print(attr.name)
+            itemData = ItemData()
+            evalObject = object.evaluated_get(depsgraph)
 
+            print("checking object " + evalObject.name)
+
+            #  if len(evalObject.data.attributes) < 2:
+            #     print( "----"+ evalObject.name + "----")
+            #    if len(evalObject.data.attributes) != 0:
+            #        for attr in evalObject.data.attributes:
+            #              print(attr.name)
+
+            #TODO add a ctor to the class and remove all this bs
+            pos = evalObject.location
+            itemData.positionX = pos.x
+            itemData.positionY = pos.y
+            itemData.positionZ = pos.z
+
+            rot = evalObject.rotation_euler
+            itemData.rotationX = rot.x
+            itemData.rotationY = rot.y
+            itemData.rotationZ = rot.z
+
+            scale = evalObject.rotation_euler
+            itemData.scaleX = scale.x
+            itemData.scaleY = scale.y
+            itemData.scaleZ = scale.z
+            itemList.append(itemData.toJSON())
+
+        else:
+            print(f"Skipping {object.name} because its not marked for export")
+
+    stringToSave = ""
+    for jsonString in itemList:
+        stringToSave+=f",{jsonString} \n"
+
+
+    file = open(filepath,'w')
+
+    file.write(stringToSave)
+    print(f"File saved to {filepath}")
     return {'FINISHED'}
 
-
-# THIS IS THE EXAMPLE ON GETING ATTRIBUTE DATA OUT OF NODE GRAPH 
+    # THIS IS THE EXAMPLE ON GETING ATTRIBUTE DATA OUT OF NODE GRAPH 
+    # just make sure to get the evaluated object
 # basePath = "G:/__Inbox/"
 # data = object_eval.data
 # CollectData(data.attributes['ID'].data,
@@ -59,33 +88,27 @@ def export_item_data(context, filepath):
 #  CollectData(data.attributes['InstanceRotation'].data,
 #             basePath + "Rotation.txt")
 
-# f = open(filepath, 'w', encoding='utf-8')
-# f.write("Hello World %s" % use_some_setting)
-# f.close()
+#f = open(filepath, 'w', encoding='utf-8')
+#f.write("Hello World %s" % use_some_setting)
+#f.close()
 
 # get up and forward rotation
 # up = obj.matrix_world.to_quaternion() @ Vector((0.0, 1.0, 0.0))
 # forward = obj.matrix_world.to_quaternion() @ Vector((0.0, 0.0, 1.0))
 
 
-def CollectData(dataList, filePath):
-    active = bpy.context.active_object
-    depsGraph = bpy.context.evaluated_depsgraph_get()
-    object_eval = active.evaluated_get(depsGraph)
 
-    with open('filePath', 'w') as f:
-        for d in dataList:
-            if type(d.value) is bpy.types.FloatVectorAttribute:
-                print(f'{d} {d.value}')
+
+
 
 
 # print(bpy.data.objects["Cube.003"].modifiers["GeometryNodes"]["Output_3_attribute_name"])
 
 
 class ItemData:
-    rotX = None
-    rotY = None
-    rotZ = None
+    positionX = None
+    positionY = None
+    positionZ = None
 
     scaleX = None
     scaleY = None
@@ -98,6 +121,7 @@ class ItemData:
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__,
                           sort_keys=True, indent=4)
+
 
 
 class ForgeItemPropertiesPanel(bpy.types.Panel):
@@ -121,18 +145,19 @@ class ForgeItemPropertiesPanel(bpy.types.Panel):
         row = layout.row()
         row.prop(obj, "forge_test_prop")
         row.prop(obj, "forge_enum")
-
+        row.prop(obj, "forge_export_toggle")
         row = layout.row()
     # row.operator("mesh.primitive_cube_add")
 
 
-class ExportSomeData(Operator, ExportHelper):  # panel for exporting data
+
+class ExportSomeData(Operator, ExportHelper): #panel for exporting data
     """Exporter to save item data for later processing into an mvar format"""
     bl_idname = "halo_forge.save_item_data"  # important since its how bpy.ops.import_test.some_data is constructed
     bl_label = "Export Halo Forge Data"
 
     # ExportHelper mixin class uses this
-    filename_ext = ".txt"
+    filename_ext = ".DCjson"
 
     filter_glob: StringProperty(
         default="*.txt",
@@ -147,7 +172,7 @@ class ExportSomeData(Operator, ExportHelper):  # panel for exporting data
         description="Example Tooltip",
         default=True,
     )
-    # Both Propertys are from the template they add buttons
+    # Both are from the template they add buttons
     # on the side of the export window
     type: EnumProperty(
         name="Example Enum",
@@ -174,18 +199,24 @@ def register():
     bpy.utils.register_class(ForgeItemPropertiesPanel)
     bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
-    # Propertys
+    #Properties
     bpy.types.Object.forge_test_prop = bpy.props.StringProperty(
         name='Object scoped prop test',
     )
 
+    bpy.types.Object.forge_export_toggle = bpy.props.BoolProperty(
+        name="Export Item",
+        description="This enables this item to be saved when exporting",
+        default=True,
+
+    )
     bpy.types.Object.forge_enum = bpy.props.EnumProperty(
-        name="",
+        name = "",
         description="Forge Enum Prop",
-        items=[
-            ('OP1', "Option 1 Name", "Test 1 Tooltip desc"),
-            ('OP2', "Option 2 Name", "Test 2 Tooltip desc"),
-            ('OP2', "Option 3 Name", "Test 3 Tooltip desc"),
+        items= [
+            ('OP1',"Option 1 Name","Test 1 Tooltip desc"),
+            ('OP2',"Option 2 Name","Test 2 Tooltip desc"),
+            ('OP2',"Option 3 Name","Test 3 Tooltip desc"),
         ]
     )
 
@@ -201,3 +232,4 @@ if __name__ == "__main__":
 
     # test call
     bpy.ops.halo_forge.save_item_data('INVOKE_DEFAULT')
+
