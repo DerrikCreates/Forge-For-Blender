efa4fab35f23392ec3a67322b4a65767659acc3d
Derrik <heyimderrik@gmail.com>
2022-09-29 09:38:16 -0400

    using object dimensions instead of scale

--- Core/Blender Addon/BlenderAddon.py
+++ Core/Blender Addon/BlenderAddon.py
@@ -55,7 +55,7 @@ def export_item_data(context, filepath):
             itemData.rotationY = rot.y
             itemData.rotationZ = rot.z
 
-            scale = evalObject.scale
+            scale = evalObject.dimensions
             itemData.scaleX = scale.x
             itemData.scaleY = scale.y
             itemData.scaleZ = scale.z
