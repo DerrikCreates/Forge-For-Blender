7aa7b6fc5fd6810305a53c6728a31e930fd5c47a
Derrik <heyimderrik@gmail.com>
2022-10-12 07:29:02 -0400

    manually setting rotation
    eventually setup Forge Constants ctors for easy creation

--- Core/BlenderData.cs
+++ Core/BlenderData.cs
@@ -1,11 +1,14 @@
 using System.Numerics;
 using BondReader;
 using BondReader.Schemas;
+using BondReader.Schemas.Generic;
+using BondReader.Schemas.Items;
 using InfiniteForgeConstants;
 using InfiniteForgeConstants.MapSettings;
 using InfiniteForgeConstants.ObjectSettings;
 using Newtonsoft.Json;
 using Serilog;
+using Vector3 = System.Numerics.Vector3;
 
 namespace ForgeTools.Core;
 
@@ -13,54 +16,70 @@ public static class BlenderData
 {
     public static void ProcessAndSave(string dataPath, string outputPath)
     {
+        
         var mapObject = new Map(MapId.CATALYST); //todo read map id from blender
+        BondSchema map = new BondSchema(mapObject);
         var fileInfo = new FileInfo(dataPath);
 
         Log.Information("Saving Mvar to {OutputPath}", outputPath);
 
-
+        var jsonString = File.ReadAllText(dataPath);
+        var blenderMap = JsonConvert.DeserializeObject<BlenderMap>(jsonString);
         // var map = new Map();
+
 
         //var mapShell = XDocument.Load(Program.EXEPath + "/ExampleMap.xml");
         string[] items = File.ReadAllText(fileInfo.FullName).Split("}", StringSplitOptions.RemoveEmptyEntries);
-        foreach (var item in items)
+        foreach (var item in blenderMap.ItemList)
         {
-            var i = item + "}";
+            NewFunction(item, mapObject, 25, new Vector3(item.RotationX, item.RotationY, item.RotationZ));
+            
+        }
+
+        void NewFunction(BlenderItem blenderItem, Map o, float offset, Vector3 rotation)
+        {
+            //forward = new Vector3(blenderItem.ForwardX, blenderItem.ForwardY, blenderItem.ForwardZ);
+            var up = new Vector3(blenderItem.UpX, blenderItem.UpY, blenderItem.UpZ);
+            rotation = Vector3.Normalize(-rotation);
+            //var rotation = new Vector3(blenderItem.RotationZ, blenderItem.RotationX, blenderItem.RotationY);
+            //rotation = Vector3.Normalize(rotation);
+            //rotation *= -1;
 
 
-            var bItem = JsonConvert.DeserializeObject<BlenderItem>(i);
-            if (bItem == null)
-                continue;
-
-
-            var forward = new Vector3(bItem.ForwardX, bItem.ForwardY, bItem.ForwardZ);
-            var up = new Vector3(bItem.UpZ, bItem.UpY, bItem.UpX);
-
-
-            Transform t = new Transform(Vector3.Zero, (up, forward));
+            Transform t = new Transform(Vector3.Zero, rotation);
             GameObject go = new GameObject(transform: t);
 
+            
 
-            go.Transform.Position.X = (bItem.PositionX); // / 10f;
-            go.Transform.Position.Y = (bItem.PositionY); // / 10f;
-            go.Transform.Position.Z = (bItem.PositionZ); /// 10f;
+            
+            
+            go.Transform.Rotation = Quaternion.CreateFromYawPitchRoll(rotation.X, rotation.Y, rotation.Z);
+            go.Transform.Position.X = (blenderItem.PositionX); // / 10f;
+            go.Transform.Position.Y = (blenderItem.PositionY); // / 10f;
+            go.Transform.Position.Z = (blenderItem.PositionZ + offset); /// 10f;
 
-            go.Transform.Scale.X = bItem.ScaleX; /// 4f;
-            go.Transform.Scale.Y = bItem.ScaleY; /// 4f;
-            go.Transform.Scale.Z = bItem.ScaleZ; // / 4f;
+            go.Transform.Scale.X = blenderItem.ScaleX; /// 4f;
+            go.Transform.Scale.Y = blenderItem.ScaleY; /// 4f;
+            go.Transform.Scale.Z = blenderItem.ScaleZ; // / 4f;
             go.Transform.IsStatic = true;
 
 
-            go.ObjectId = ObjectId.PRIMITIVE_CONE;
+            go.ObjectId = (ObjectId)blenderItem.ItemId;
 
-            mapObject.GameObjects.Add(go);
+            
+            ItemSchema itemSchema = new ItemSchema(go);
+            itemSchema.Forward = new BondReader.Schemas.Generic.Vector3(blenderItem.ForwardX, blenderItem.ForwardY, blenderItem.ForwardZ);
+            itemSchema.Up = new BondReader.Schemas.Generic.Vector3(blenderItem.UpX, blenderItem.UpY, blenderItem.UpZ);
+
+            map.Items.AddLast(itemSchema);
             //  XMLHelper.AddObject(mapShell, mapItem.GameObject);
         }
 
+    
+        
+       
 
-        BondSchema map = new BondSchema(mapObject);
-
-        BondHelper.WriteBond(map, outputPath + "/BlenderMap.mvar");
+        BondHelper.WriteBond(map, outputPath + "/Test.mvar");
         //mapShell.WriteTo(XmlWriter.Create(Program.EXEPath + "/Test.xml"));
     }
 }
