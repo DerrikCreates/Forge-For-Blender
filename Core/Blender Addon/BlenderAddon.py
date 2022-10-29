22d4c0f46f0357027d0d15caf167c86f7f350319
Derrik <heyimderrik@gmail.com>
2022-10-28 21:20:25 -0400

    0.1.0 blender addon

--- Core/Blender Addon/BlenderAddon.py
+++ Core/Blender Addon/BlenderAddon.py
@@ -6,6 +6,7 @@ bl_info = {
     "author": "DerrikCreates.com",
     "description": "export forge items for latter processing into a .mvar",
     "blender": (3, 3, 0),
+    "version":(0,1,0),
     "location": "View3D",
     "warning": "",
     "category": "Generic"
